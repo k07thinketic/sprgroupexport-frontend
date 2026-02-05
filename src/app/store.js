6976a1f7b004fbdf882c9d66716769d4f3000013
priyanshi vaghani <priyanshivaghani.thinketic@gmail.com>
2026-02-05 11:41:35 +0530

    feat: implement parallax banner

--- src/app/store.js
+++ src/app/store.js
@@ -17,6 +17,7 @@ import reviewsReducer from '../features/reviews/reviewsSlice'
 import stockReducer from '../features/stock/stockSlice'
 import caratReducer from '../features/carat/caratSlice'
 import settingsReducer from '../features/setting/settingSlice'
+import parallaxBannerReducer from '../features/parallax-banner/parallaxBannerSlice'
 
 export const store = configureStore({
   reducer: {
@@ -27,6 +28,7 @@ export const store = configureStore({
     categoryDetails: categoryDetailsReducer,
     categories: categoriesReducer,
     settings: settingsReducer,
+    parallaxBanner: parallaxBannerReducer,
     user: userReducer,
     shippingAddress: shippingAddressReducer,
     shipping: shippingReducer,
